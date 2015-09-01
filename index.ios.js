96aa8133d5063e5548155839ea76e98017658077
Lohit Talasila <tlohit@hotmail.com>
2015-09-01 15:56:25 -0400

    added files

---
+++ index.ios.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var React = require('react-native');
+var Featured = require('./Products');
+var Search = require('./Search');
+
+var {
+    AppRegistry,
+    TabBarIOS,
+    Component
+   } = React;
+
+class SampleApp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'products'
+        };
+    }
+
+    render() {
+        return (
+            <TabBarIOS selectedTab={this.state.selectedTab}>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'Products'}
+                    icon={{uri:'featured'}}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'Products'
+                        });
+                    }}>
+                    <Featured/>
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'search'}
+                    icon={{uri:'search'}}
+                    onPress={() => {
+                        this.setState({
+                            selectedTab: 'search'
+                        });
+                    }}>
+                    <Search/>
+                </TabBarIOS.Item>
+            </TabBarIOS>
+        );
+    }
+}
+
+AppRegistry.registerComponent('SampleApp', () => SampleApp);
